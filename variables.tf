16f3bcdaf5768c9c6fba8471de55ad795241909a
sulig312 <sulig321@gmail.com>
2025-09-13 11:11:30 -0400

    format error included in provider.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,29 @@
 variable "ami_id" {
-    description = "ec2 ami ID"
-    default = "ami-0634f3c109dcdc659"
-  
+  description = "ec2 ami ID"
+  default     = "ami-0634f3c109dcdc659"
+
 }
 
 variable "instance_type" {
-    description = "ec2 instance type"
-    default = "t2.micro"
-  
+  description = "ec2 instance type"
+  default     = "t2.micro"
+
 }
 
 variable "region" {
-    description = "aws region"
-    
-  
+  description = "aws region"
+
+
 }
 
 variable "access_key" {
-    description = "aws user access key"
-    
-  
+  description = "aws user access key"
+
+
 }
 
 variable "secret_key" {
-    description = "aws user secret key"
-    
-  
+  description = "aws user secret key"
+
+
 }
